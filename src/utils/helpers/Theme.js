0388a8bc1580bed7e0b20242577e9da46f98c769
kelvinanderson10 <kelvinbusiness10@gmail.com>
2024-03-14 17:06:02 +0700

    clean up ignored files

--- src/utils/helpers/Theme.js
+++
@@ -1,8 +0,0 @@
-export const isCurrentDarkMode = () => {
-    const currentTheme = localStorage.getItem('theme')
-    if (currentTheme == 'light' || currentTheme == null) {
-        return false
-    }
-    return true
-}
-
